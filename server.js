235eb7298d8ca894d3a2cff0163cdc6f337ec4ca
Firman <fhaya.firman@gmail.com>
2021-04-13 12:04:45 +0700

    add nodemon and eslint library

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost'
-    });
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost'
+  });
 
-    server.route(routes);
+  server.route(routes);
 
-    await server.start();
+  await server.start();
 
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 }
 
 init();
