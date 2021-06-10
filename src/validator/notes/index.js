06e88665a9118bd4dcf45e8ce8497365d1338bb3
Firman <fhaya.firman@gmail.com>
2021-06-10 09:45:14 +0700

    fixed bugs

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -1,5 +1,5 @@
-const InvariantError = require("../../exceptions/InvariantError");
-const { NotePayloadSchema } = require("./schema")
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayloadSchema } = require('./schema')
 
 const NotesValidator = {
   validateNotePayload: (payload) => {
@@ -8,7 +8,7 @@ const NotesValidator = {
     if(validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
-  }
-}
+  },
+};
 
 module.exports = NotesValidator;
